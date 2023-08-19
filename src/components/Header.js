0043ff09b3b8f9be44015ed1d44637904507fdbf
Nahla31 <nahlagamal3125@gmail.com>
2023-08-19 03:14:54 +0300

    Meme Generator

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="./images/Troll face.png" className="header--img"></img>
+      <h2 className="header--title">Meme Generator</h2>
+      <p className="header--project">React Course - Project 3</p>
+    </header>
+  );
+}
